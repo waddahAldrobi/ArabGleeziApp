55eb79ad9f6e41cee232b7dec8bba900261bc08a
Waddah Aldrobi <waddahaldrobi@Waddahs-MacBook-Pro.local>
2017-12-11 02:05:48 -0500

    getting somewhere

--- arabGleezi/ViewController.swift
+++ arabGleezi/ViewController.swift
@@ -12,51 +12,58 @@ class ViewController: UIViewController {
     @IBOutlet weak var userInput: UITextField!
     @IBOutlet weak var userOutput: UILabel!
     var strLength = 0
-    
+    var word = ""
     @IBAction func isEditing(_ sender: Any) {
         userOutput.text = userInput.text
-        print ("hi")
-        print (strLength)
+        print ("str " + String(strLength) )
         strLength = userInput.text?.count ?? 0
         var iChars = Array(userInput.text!)
+        var iCharsArabic = Array(userInput.text!)
         
-        //print (iChars[0])
         if (strLength <= 0){
             userOutput.text = ""
         }
         else{  //figure out how to do extra letter, and start determining location of letter in word
             for i in 0 ... strLength-1{
-                (iChars[i] == "a" || iChars[i] == "e") ? iChars[i] = "ا" : nil  /*or e*/
-                iChars[i] == "2" ? iChars[i] = "أ" : nil      //ء أ آ ؤ إ ئ
+                (iChars[i] == "a" || iChars[i] == "e") ? iCharsArabic[i] = "ا" : nil  /*or e*/
+                iChars[i] == "2" ? iCharsArabic[i] = "أ" : nil      //ء أ آ ؤ إ ئ
                 
-                (iChars[i] == "b" || iChars[i] == "p")  ? iChars[i] = "ب" : nil
-                iChars[i] == "t" ? iChars[i] = "ت" : nil
-                iChars[i] == "s" ? iChars[i] = "ث" : nil // iChars[i] ="th" if statement
-                (iChars[i] == "j" || iChars[i] == "g")  ? iChars[i] = "ج" : nil  // "dj" if
-                iChars[i] == "7" ? iChars[i] = "ح" : nil // also h
-                iChars[i] == "5" ? iChars[i] = "خ" : nil  // "kh" "7'"
-                iChars[i] == "d" ? iChars[i] = "د" : nil
-                iChars[i] == "z" ? iChars[i] = "ذ" : nil // "th" "dh"  ## z can be ز
-                iChars[i] == "r" ? iChars[i] = "ر" : nil
-                iChars[i] == "s" ? iChars[i] = "س" : nil //
-                iChars[i] == "9" ? iChars[i] = "ص" : nil // ## can also be s
-                iChars[i] == "6" ? iChars[i] = "ط" : nil // ## can also be t
+                (iChars[i] == "b" || iChars[i] == "p")  ? iCharsArabic[i] = "ب" : nil
+                iChars[i] == "t" ? iCharsArabic[i] = "ت" : nil
+                iChars[i] == "s" ? iCharsArabic[i] = "ث" : nil // iChars[i] ="th" if statement
+                (iChars[i] == "j" || iChars[i] == "g")  ? iCharsArabic[i] = "ج" : nil  // "dj" if
+                if (iChars[i] == "’") { // add or '
+                    print ("in")
+                    if iChars[i-1] == "7" {
+                        iCharsArabic[i-1] = "خ"
+                        iChars.removeLast()
+                        iChars.append("")
+                    }
+                }
+                iChars[i] == "7" ? iCharsArabic[i] = "ح" : nil // also h
+                iChars[i] == "5" ? iCharsArabic[i] = "خ" : nil  // "kh" "7'"
+                iChars[i] == "d" ? iCharsArabic[i] = "د" : nil
+                iChars[i] == "z" ? iCharsArabic[i] = "ذ" : nil // "th" "dh"  ## z can be ز
+                iChars[i] == "r" ? iCharsArabic[i] = "ر" : nil
+                iChars[i] == "s" ? iCharsArabic[i] = "س" : nil // this is WRONG
+                iChars[i] == "9" ? iCharsArabic[i] = "ص" : nil // ## can also be s
+                iChars[i] == "6" ? iCharsArabic[i] = "ط" : nil // ## can also be t
+            
                 
                 
+                (iChars[i] == "o" || iChars[i] == "3") ? iCharsArabic[i] = "ع" : nil
+                (iChars[i] == "f" || iChars[i] == "v") ? iCharsArabic[i] = "ف" : nil
                 
-                (iChars[i] == "o" || iChars[i] == "3") ? iChars[i] = "ع" : nil
-                (iChars[i] == "f" || iChars[i] == "v") ? iChars[i] = "ف" : nil
                 
+                iChars[i] == "m" ? iCharsArabic[i] = "م" : nil
                 
-                iChars[i] == "m" ? iChars[i] = "م" : nil
-                
-                (iChars[i] == "8" || iChars[i] == "q") ? iChars[i] = "ق" : nil // ## g and 2 too
-                iChars[i] == "k" ? iChars[i] = "ك" : nil  // maybe g ?
-                iChars[i] == "l" ? iChars[i] = "ل" : nil
-                iChars[i] == "n" ? iChars[i] = "ن" : nil
-                (iChars[i] == "h") ? iChars[i] = "ه" : nil // ## e,ah and eh , maybe a
-                (iChars[i] == "w" || iChars[i] == "u") ? iChars[i] = "و" : nil //ou oo, maybe o
-                (iChars[i] == "y" || iChars[i] == "i") ? iChars[i] = "ي" : nil //ee ei ai, maybe a
+                (iChars[i] == "8" || iChars[i] == "q") ? iCharsArabic[i] = "ق" : nil // ## g and 2 too
+                iChars[i] == "k" ? iCharsArabic[i] = "ك" : nil  // maybe g ?
+                iChars[i] == "l" ? iCharsArabic[i] = "ل" : nil
+                iChars[i] == "n" ? iCharsArabic[i] = "ن" : nil
+                (iChars[i] == "h") ? iCharsArabic[i] = "ه" : nil // ## e,ah and eh , maybe a
+                (iChars[i] == "w" || iChars[i] == "u") ? iCharsArabic[i] = "و" : nil //ou oo, maybe o
+                (iChars[i] == "y" || iChars[i] == "i") ? iCharsArabic[i] = "ي" : nil //ee ei ai, maybe a
                 
                 /*
                  "sh" or "ch" => ش
@@ -64,19 +71,20 @@ class ViewController: UIViewController {
                  "z" "th" "dh" "6'" => ظ
                  "gh" "3'" => غ
                  "a" "e" "eh" "at" "et" => ة
-                 
- */
-                
-                
-                
+                 */
             }
-            userOutput.text = String(iChars)
+            
+            userOutput.text = String(iCharsArabic)
         }
     }
     
-    func trasnlate (iChars: [String]){
-        
-    }
+   /* func trasnlate (iChars: [String]){
+      later implement
+     
+     if (iChars[i] == " "){
+     print (iChars[i-1])
+     }
+    }*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
